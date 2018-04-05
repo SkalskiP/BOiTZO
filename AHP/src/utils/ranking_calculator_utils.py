018ccc983c3bf3fb19cdffce7fa9729be165ab80
SkalskiP <pskalski@student.agh.edu.pl>
2018-04-05 18:46:05 +0200

    git ignore added

---
+++ AHP/src/utils/ranking_calculator_utils.py
@@ -0,0 +1,9 @@
+# ======================================================================================================================
+# PROJECT NAME:             Easy AHP Tool
+# FILE NAME:                FileUtils
+# FILE VERSION:             1.0
+# DATE:                     17.03.2018
+# AUTHOR:                   Piotr Skalski [github.com/SkalskiP]
+# ======================================================================================================================
+# File contains set of static methods responsible for parsing tree structure to JSON as well as saving it to file
+# ======================================================================================================================
